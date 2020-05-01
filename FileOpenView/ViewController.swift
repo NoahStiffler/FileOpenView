34bddeff9a886ea17831cc075d92fc902c386a46
Noah Stiffler <stifflernoah@gmail.com>
2020-05-01 14:17:06 -0400

    Initial Commit

---
+++ FileOpenView/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  FileOpenView
+//
+//  Created by Noah Stiffler on 5/1/20.
+//  Copyright © 2020 Noah Stiffler. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
